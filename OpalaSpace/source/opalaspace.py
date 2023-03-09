faa3fcbe5525165052a05e84aaa276de4b46f313
Ruan <ruanvictorp22111@gmail.com>
2023-03-09 19:35:25 -0300

    contribuindo com o jogo.

--- OpalaSpace/source/opalaspace.py
+++ OpalaSpace/source/opalaspace.py
@@ -16,17 +16,17 @@ altura = 720
 screen = pygame.display.set_mode((largura,altura))
 pygame.display.set_caption('Jogo Opala Space - IFPI campus Pedro II')
 
-img_fundo = pygame.image.load('figs/fundo.jpg').convert_alpha()
+img_fundo = pygame.image.load('figs/RaqueMan.jpg').convert_alpha()
 img_fundo = pygame.transform.scale(img_fundo, (largura,altura))
 
-img_alien = pygame.image.load('figs/alien.png').convert_alpha()
+img_alien = pygame.image.load('figs/Manuel.PNG').convert_alpha()
 img_alien = pygame.transform.scale(img_alien, (50,50))
 
-img_aviao = pygame.image.load('figs/aviao.png').convert_alpha()
+img_aviao = pygame.image.load('figs/Thiago.PNG').convert_alpha()
 img_aviao = pygame.transform.scale(img_aviao, (50,50))
 img_aviao = pygame.transform.rotate(img_aviao, -90)
 
-img_missil = pygame.image.load('figs/missil.png')
+img_missil = pygame.image.load('figs/Willame.PNG')
 img_missil = pygame.transform.scale(img_missil,(25,25))
 img_missil = pygame.transform.rotate(img_missil, -45)
 
